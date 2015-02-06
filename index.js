932e76a6cf4904d93465a043fc415bdfb265eac5
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-02-06 11:29:55 +0100

    Using the dux-dispatcher-connection

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
 #!/usr/bin/env node
 var args = require('minimist')(process.argv.slice(2), {
     default : {
-        'firebase-url'    : process.env['FIREBASE_URL'],
-        'firebase-path'   : '/',
-        'firebase-secret' : process.env['FIREBASE_SECRET'],
-        'dispatcher'      : 'dux-dispatcher.dux.test'
+        'firebase-url'       : process.env['FIREBASE_URL'],
+        'firebase-path'      : '/',
+        'firebase-secret'    : process.env['FIREBASE_SECRET'],
+        'dispatcher-host'    : process.env['DISPATCHER_HOST'],
+        'dispatcher-host'    : process.env['DISPATCHER_HOST'],
+        'retry-interval'     : process.env['RETRY_INTERVAL']     || 5000,
+        'connection-timeout' : process.env['CONNECTION_TIMEOUT'] || 1000
     }
 })
 var utils           = require('./utils')
 var fb_conn         = require('./firebase-connection')
-var dp_conn         = require('./dispatcher-connection')
+var dp_conn         = require('dux-dispatcher-connection')
 var StateDispatcher = require('./state-dispatcher')
 var StateApi        = require('./state-api')
 
 utils.validateFirebaseArgs(args)
 
 var firebase_connection = fb_conn(args)
-var dispatcher_connection = dp_conn(args)
+var dispatcher_connection = dp_conn({
+    host     : args['dispatcher-host'],
+    port     : args['dispatcher-port'],
+    interval : args['retry-interval'],
+    timeout  : args['connection-timeout']
+})
 var dispatcher = new StateDispatcher(firebase_connection, dispatcher_connection)
 var api        = new StateApi({}, dispatcher) 
 
 var check_state = function() {
-    var ready = (firebase_connection.ready() && dispatcher_connection.ready())
+    var ready = (firebase_connection.ready() && dispatcher_connection.ready)
     if (ready && !dispatcher.running) { dispatcher.start(); api.start() } 
     if (!ready && dispatcher.running) dispatcher.stop()
 }
 firebase_connection.keepAlive(5000, function(err) {
-    if (err) console.log('Firebase Connection Error:',err)
+    if (err) console.error('Firebase Connection Error:',err)
     check_state()
 })
-dispatcher_connection.keepAlive(5000,function(err) {
-    if (err) console.log('Dispatcher Connection Error:',err)
-    check_state()
-})
+dispatcher_connection.listen()
 
